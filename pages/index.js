e8332a954b06e49f7b68d014b7b123f824372d86
Gabriel Victor <gabrielvictor89@gmail.com>
2025-12-02 23:10:45 +0000

    feat: create home page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+    <>
+        <h1>Welcome to TabNews Clone!</h1>
+        <h2>Your learning project for full-stack development.</h2>
+    </>
+ );
+}
+
+export default Home;
